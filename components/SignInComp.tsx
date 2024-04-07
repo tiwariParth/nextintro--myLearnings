9386ea785dcd39664da30674397901199209930e
tiwariParth <243mjparth@gmail.com>
2024-04-07 18:44:40 +0530

    feat: :sparkles: server and client components
    Learnt what are server and client components in nextjs how to differentiat them. So by defualt nextJS consider all the components as server side and to make it client side "use client" on the top of the code is the way to do it.

--- components/SignInComp.tsx
+++ components/SignInComp.tsx
@@ -1,6 +1,10 @@
+"use client";
 import React from "react";
 
 const SignInComp = () => {
+  const handler = () => {
+    console.log("Sign in clicked");
+  };
   return (
     <div className="h-screen flex justify-center flex-col">
       <div className="flex justify-center">
@@ -25,6 +29,7 @@ const SignInComp = () => {
               <button
                 type="button"
                 className="mt-8 w-full text-white bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
+                onClick={handler}
               >
                 Sign in
               </button>
